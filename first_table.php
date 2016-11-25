2d8fc620f43825bd9c0297639dde86e138feeef2
sewan <kido184@hotmail.com>
2016-11-25 18:05:40 +0000

    hello

--- first_table.php
+++ first_table.php
@@ -2,10 +2,10 @@
 include 'DBCONNECT.php';
 
 $db = new mysqli(
-"hostname",
-"username",
-"password",
-"dbname"
+"us-cdbr-azure-southcentral-f.cloudapp.net",
+"b762b602ac9c7b",
+"360b7be5",
+"cm3028rgu"
 );
 // test if connection was established, and print any errors
 if($db->connect_errno){
